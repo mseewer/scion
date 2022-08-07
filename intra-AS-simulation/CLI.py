3aae1dc27d70d24dcc0428f50ffad909d4d6f156
Marco Seewer <mseewer@student.ethz.ch>
2022-08-08 00:43:00 +0200

    add labels to graph + fix multigraph edges

--- intra-AS-simulation/CLI.py
+++ intra-AS-simulation/CLI.py
@@ -9,7 +9,50 @@ from mininet.cli import CLI
 from test_SCION_in_mininet import test_SCION_ping, test_SCION_bwtest_client_server
 
 
+def plot_topology(AS):
+    """Plot the intra-AS topology
+
+    Draws all nodes and edges of internal topology.
+    Also annotates edges with link properties.
+    """
+    # set figure title
+    plt.figure(num=f'Intra-AS topology of AS: {AS.ISD_AS_id}')
+    G = AS.graph
+    try:
+        pos = nx.planar_layout(G)
+    except NetworkXException:
+        pos = nx.spring_layout(G)
+    # draw nodes
+    nx.draw_networkx_nodes(G, pos, node_color='#8691fc')
+    # add node names
+    nx.draw_networkx_labels(G, pos)
+    ax = plt.gca()
+    for e in G.edges:
+        # manually connect 2 nodes
+        # this is needed, since we can have multiple edges between 2 nodes
+        ax.annotate("",
+                    xy=pos[e[0]], xycoords='data',
+                    xytext=pos[e[1]], textcoords='data',
+                    arrowprops=dict(arrowstyle="-", alpha=0.6,
+                                    shrinkA=10, shrinkB=10,
+                                    # creates line with a specific curvature/radius
+                                    connectionstyle=f"arc3,rad={-0.1*e[2]}",
+                                    ),
+                    )
+        # add label to edge
+        label = G.get_edge_data(*e)['label']
+        # text position: center of edge   + correction when multiple edges between two nodes
+        text_pos = (pos[e[0]] + pos[e[1]] + [0.08*e[2], 0.08*e[2]]) / 2
+        ax.annotate(label, xy=text_pos, xycoords='data',
+                    xytext=text_pos, textcoords='data',
+                    fontsize=10)
+
+    plt.axis('off')
+    plt.show()
+
+
 def AS_CLI(networks, ASes, AS):
+    """CLI for interacting with the AS"""
     while True:
         try:
             output('--------------------------------------------------\n')
@@ -18,7 +61,7 @@ def AS_CLI(networks, ASes, AS):
             output('[1] Test SCION ping\n')
             output('[2] Start SCION bandwidth test\n')
             output('[3] Start Mininet CLI\n')
-            output('[4] Draw intra AS topology\n')
+            output('[4] Draw intra-AS topology\n')
             option = input('Enter option: ').strip()
             if option == '0':
                 break
@@ -41,16 +84,7 @@ def AS_CLI(networks, ASes, AS):
             elif option == '4':
                 info('### Drawing intra AS topology ###\n')
                 try:
-                    # set figure title
-                    plt.figure(num=f'Intra-AS topology of AS: {AS.ISD_AS_id}')
-                    try:
-                        nx.draw_planar(AS.graph, node_color='#8691fc', with_labels=True)
-                    except NetworkXException:
-                        # Graph is not planar
-                        nx.draw(AS.graph, node_color='#8691fc', with_labels=True)
-                    # TODO: add edge labels
-                    # problem with multigraph, how to label multiple edges between 2 nodes?
-                    plt.show()
+                    plot_topology(AS)
                 except Exception as e:
                     output(f'Drawing failed: {e}\n')
             else:
